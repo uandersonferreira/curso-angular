d2dd47548b0c245ceff4db946de0b88f2ab10721
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-09-08 15:58:46 -0300

    js e typescript diferença

---
+++ angular-loiane/hello-typescript/main.ts
@@ -0,0 +1,27 @@
+var minhaVar = 'minha variável';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+//ES 6 OU  ES 2015 
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map((valor => {
+    valor * 2
+}));
+
+class Matematica{
+    soma(x,y){
+        return x+ y;
+    }
+}
+
+var n1: any = 'ndfkjds';
+n1 = 4;
