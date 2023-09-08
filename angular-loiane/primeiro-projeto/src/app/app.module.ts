d3caf02d46df767f6c3920038d72093bef8d5922
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-09-08 15:05:54 -0300

    criando um component manualmente

--- angular-loiane/primeiro-projeto/src/app/app.module.ts
+++ angular-loiane/primeiro-projeto/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
