1adb67745b85157d3cf4e690dca6ed25f4187b80
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-09-19 21:35:52 -0300

    aula-16-dom-and-template-@ViewChild

--- angular-loiane/data-binding/src/app/output-property/output-property.component.ts
+++ angular-loiane/data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -9,21 +9,25 @@ export class OutputPropertyComponent implements OnInit{
 
   @Input()
   valor: number = 0;
+  @Output() mudouValor = new EventEmitter;
+
+  // @ts-ignore
+  @ViewChild('campoInput') campoValorInput: ElementRef;
 
   incrementa(){
-    this.valor++
+    console.log("INCREMENTO: "+this.campoValorInput.nativeElement.value);
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa(){
-    this.valor--
-    if(this.valor < 0){
-      this.valor = 0;
-    }
+    console.log("DECREMENTO: "+this.campoValorInput.nativeElement.value);
+    this.campoValorInput.nativeElement.value--;
+    this.mudouValor.emit({novoValor: this.valor});
   }
 
 
   constructor(){}
   ngOnInit(): void {}
-
 
 }//class
