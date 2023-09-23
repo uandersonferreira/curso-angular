3a5ae688310f3a0655971bb69efd1261c1cb0a1a
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-09-23 18:21:02 -0300

    Diretiva *ngIf

--- angular-loiane/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ angular-loiane/diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
   styleUrls: ['./diretiva-ngif.component.css']
 })
-export class DiretivaNgifComponent {
+export class DiretivaNgifComponent implements OnInit{
+  cursos: string[] = ["Angular 2"];
+  mostrarCursos: boolean = false;
 
-}
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+}//class
